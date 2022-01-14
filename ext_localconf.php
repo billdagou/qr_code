22982249dbfc005e4250c63075c9ac91fcb787be
Bill.Dagou <billdagou@gmail.com>
2022-01-14 11:36:53 +0800

    Update

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,7 +1,7 @@
 <?php
 defined('TYPO3_MODE') || die();
 
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/install']['update']['qr_code'] = \Dagou\Qrcode\Updates\QrCode::class;
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/install']['update']['qr_code'] = \Dagou\QrCode\Updates\QrCode::class;
 
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['fluid']['namespaces']['qrcode'] = [
     'Dagou\\QrCode\\ViewHelpers',
