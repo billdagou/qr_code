f48c48caa4dd8f9d02a9b4b395e51c1fcb5e4b2b
Bill.大狗 <billdagou@gmail.com>
2023-11-23 17:43:45 +0800

    Upgrade to v5.0.0 and TYPO3 v12

--- ext_emconf.php
+++ ext_emconf.php
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = [
     'autoload' => [
         'classmap' => [
             'Classes',
-            'Resources/Private/MkDir',
+            'Resources/Private/QrCode',
         ],
     ],
 ];
